74a81b2652d4bd7b77a1777b6f2f846efeecefb8
Ben Evans <benjamin.john.evans@gmail.com>
2017-04-12 20:02:57 +0200

    Initial functions

---
+++ src/main/java/dec64/DEC64.java
@@ -0,0 +1,143 @@
+package dec64;
+
+/**
+ *
+ * @author ben
+ */
+public final class DEC64 {
+
+    public static boolean isNaN() {
+        return false;
+    }
+
+    public static long coefficient(long number) {
+        return 0;
+    }/* coefficient */
+
+
+    public static long exponent(long number) {
+        return 0;
+    }/* exponent */
+
+
+    public static long equal(long comparahend, long comparator) {
+        return 0;
+    }/* comparison */
+
+
+    public static boolean isNaN(long number) {
+        return false;
+    }/* comparison */
+
+
+    public static boolean isInteger(long number) {
+        return false;
+    }/* comparison */
+
+
+    public static boolean isZero(long number) {
+        return false;
+    }/* comparison */
+
+
+    public static long less(long comparahend, long comparator) {
+        return 0;
+    }/* comparison */
+
+
+    public static long abs(long number) {
+        return 0;
+    }/* absolution */
+
+
+    public static long add(long augend, long addend) {
+        return 0;
+    }/* sum */
+
+
+    public static long ceiling(long number) {
+        return 0;
+    }/* integer */
+
+
+    public static long dec(long minuend) {
+        return 0;
+    }/* decrementation */
+
+
+    public static long divide(long dividend, long divisor) {
+        return 0;
+    }/* quotient */
+
+
+    public static long floor(long dividend) {
+        return 0;
+    }/* integer */
+
+
+    public static long half(long dividend) {
+        return 0;
+    }/* quotient */
+
+
+    public static long inc(long augend) {
+        return 0;
+    }/* incrementation */
+
+//    public static long
+//
+//    int (long number) {
+//    }/* integer */
+
+
+    public static long integer_divide(long dividend, long divisor) {
+        return 0;
+    }/* quotient */
+
+
+    public static long modulo(long dividend, long divisor) {
+        return 0;
+    }/* modulation */
+
+
+    public static long multiply(long multiplicand, long multiplier) {
+        return 0;
+    }/* product */
+
+
+    public static long neg(long number) {
+        return 0;
+    }/* negation */
+
+
+    public static long of(long coefficient, long exponent) {
+        return 0;
+    }/* number */
+
+
+    public static long normal(long number) {
+        return 0;
+    }/* normalization */
+
+
+    public static long not(long value) {
+        return 0;
+    }/* notation */
+
+
+    public static long round(long number, long place) {
+        return 0;
+    }/* quantization */
+
+
+    public static long signum(long number) {
+        return 0;
+    }/* signature */
+
+
+    public static long subtract(long minuend, long subtrahend) {
+        return 0;
+    }/* difference */
+
+
+}
