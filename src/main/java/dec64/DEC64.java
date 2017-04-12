3f604e4108aba28e47033ba87ae8fb0f297362eb
Ben Evans <benjamin.john.evans@gmail.com>
2017-04-13 00:05:56 +0200

    First test passes

--- src/main/java/dec64/DEC64.java
+++ src/main/java/dec64/DEC64.java
@@ -1,9 +1,16 @@
 package dec64;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  *
  * @author kittylyst
  */
+@Target({ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface DEC64 {
     
 }
---
+++ src/test/java/dec64/SimpleTest.java
@@ -0,0 +1,29 @@
+package dec64;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author ben
+ */
+public class SimpleTest {
+
+    @Test
+    public void simpleAdd() {
+        @DEC64 long minusOne = Math64.DEC64_NEGATIVE_ONE;
+        @DEC64 long one = Math64.DEC64_ONE;
+
+        assertEquals("-1 + 1 should equal 0", Math64.DEC64_ZERO, Math64.add(one, minusOne));
+    }
+
+    @Test
+    public void simpleMult() {
+        @DEC64 long ten = Math64.of(10, (byte)0);
+        // FIXME Need proper equality method to deal with multiple representations
+        //assertTrue("0.1 * 1 should equal 1", Math64.equals(Math64.DEC64_ONE, Math64.multiply(ten, Math64.DEC64_POINT_ONE)));
+        assertEquals("0.1 * 1 should equal 1", Math64.DEC64_ONE, Math64.multiply(ten, Math64.DEC64_POINT_ONE));
+    }
+
+    
+}
