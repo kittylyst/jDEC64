6a85312028cb3c94789db9d5c707339f5c345b48
Ben Evans <benjamin.john.evans@gmail.com>
2017-05-20 11:31:24 +0200

    Start adding in Pasrsing support

--- src/main/java/dec64/repl/REPL64.java
+++ src/main/java/dec64/repl/REPL64.java
@@ -1,5 +1,7 @@
 package dec64.repl;
 
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
 
 /**
  *
@@ -8,16 +10,23 @@ package dec64.repl;
 public final class REPL64 {
 
     private final StackDec64 interpStack = new StackDec64(255);
-    
-    
-    
+
     public static void main(String[] args) {
         final REPL64 r = new REPL64();
-        r.loop();
+        r.parseLine(args[0]);
+//        r.loop();
+    }
+
+    public void parseLine(final String l) {
+        final DEC64ReplLexer lexer = new DEC64ReplLexer(new ANTLRInputStream(l));
+        final CommonTokenStream tokens = new CommonTokenStream(lexer);
+        final DEC64ReplParser parser = new DEC64ReplParser(tokens);
+        
+        
     }
 
     private void loop() {
-        
+
     }
-    
+
 }
