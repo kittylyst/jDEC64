f0b0721b92891409853126bb591f44cb6f97eff7
Allan de Queiroz <allandequeiroz@gmail.com>
2017-06-03 12:28:45 +0100

    root implementation

--- src/main/java/dec64/Math64.java
+++ src/main/java/dec64/Math64.java
@@ -148,4 +148,48 @@ public class Math64 {
     long tan(@DEC64 long radians) {
         return divide(sin64(radians), cos(radians));
     }
+
+    // FIXME : Implemented in another PR
+    public static @DEC64
+    long raise(@DEC64 long coefficient, @DEC64 long exponent) {
+        return DEC64_NAN;
+    }
+
+    public static @DEC64
+    long root(@DEC64 long degree, @DEC64 long radicand) {
+        @DEC64 long result;
+        degree = normal(degree);
+        if (isNaN(radicand) || isZero(degree) ||
+              degree < 0 || (radicand < 0 && (coefficient(degree) & 1) == 0)) {
+            return DEC64_NAN;
+        }
+        if (isZero(radicand)) {
+            return DEC64_ZERO;
+        }
+        if (degree == DEC64_TWO) {
+            sqrt(radicand);
+        }
+        @DEC64 long degree_minus_one = dec(degree);
+        result = DEC64_ONE;
+        @DEC64 long prosult = DEC64_NAN;
+        while (true) {
+            @DEC64 long progress = divide(
+                  add(
+                        multiply(result, degree_minus_one),
+                        divide(
+                              radicand,
+                              raise(result, degree_minus_one)
+                        )
+                  ), degree
+            );
+            if (progress == result) {
+                return result;
+            }
+            if (progress == prosult) {
+                return half(add(progress, result));
+            }
+            prosult = result;
+            result = progress;
+        }
+    }
 }
